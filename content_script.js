4b7817eb490c31df28da6b4e92f5ab23d7a93424
wuwi <forward.alba@gmail.com>
2014-06-26 15:13:16 +0200

    added target="_blank" to tooltip links

--- content_script.js
+++ content_script.js
@@ -25,18 +25,18 @@ $(document).ready(function() {
 			cahoots_content += ' wurden die folgenden Einträge gefunden</p>';
 			cahoots_content += '<section class="cahoots_middle"><ul id="cahoots_list">';
 			$.each(db[id].cahoots, function(i,v){
-            	cahoots_content += '<li class="cahoots_item"><a title="Mehr Infos zu dieser Organisation" href="';
+            	cahoots_content += '<li class="cahoots_item"><a target="_blank" title="Mehr Infos zu dieser Organisation" href="';
             	cahoots_content += v.more_info;
             	cahoots_content += '">';
             	cahoots_content += v.name;
-            	cahoots_content += '</a><a class="quelle" href="';
+            	cahoots_content += '</a><a target="_blank" class="quelle" href="';
             	cahoots_content += v.src;
             	cahoots_content += '">Quelle</a></li>';
             });
 	        cahoots_content += '</ul></section>';
 	        cahoots_content += '<section class="cahoots_footer">';
-	        cahoots_content += '<a href="http://jonasbergmeier.net/cahoots/contribute.html"><button class="cahoots_button">Informationen eintragen</button></a>';
-	        cahoots_content += '<a href="http://jonasbergmeier.net/cahoots/XYZ.html"><button class="cahoots_button">Fehler melden</button></a>';
+	        cahoots_content += '<a target="_blank" href="http://jonasbergmeier.net/cahoots/contribute.html"><button class="cahoots_button">Informationen eintragen</button></a>';
+	        cahoots_content += '<a target="_blank" href="http://jonasbergmeier.net/cahoots/XYZ.html"><button class="cahoots_button">Fehler melden</button></a>';
 	        cahoots_content += '</section>';
 
 	        return callback(cahoots_content);
