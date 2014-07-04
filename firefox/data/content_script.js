7569efd8ee670b774c855ace0f920af8a2ebef68
wuwi <forward.alba@gmail.com>
2014-07-04 16:01:01 +0200

    firefox changes

--- firefox/data/content_script.js
+++ firefox/data/content_script.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
-	console.log("init");
 	$.each(author, function(key, value) {
 		$("p").highlight(key, {caseSensitive: false, className: value });
 	});
-
 
 	$('span[class*=CahootsID]').tooltipster({
 		interactive: true,
@@ -17,36 +15,11 @@ $(document).ready(function() {
 		functionBefore: function(origin, continueTooltip) {
 			continueTooltip();
 			var id = $(this).attr('class').replace(' tooltipstered','');
-			cahoots_generate(id, function(){
+			console.log("sent id to main.js");
+			self.port.emit('gotID', id);
+			self.port.on('gotContent', function(cahoots_content){
 				origin.tooltipster('content', cahoots_content);
 			});
 		}
 	});
-
-
-	function cahoots_generate(id, callback) {
-		$.getJSON('db.json', function(db) {
-			cahoots_content = '<p class="cahoots_top">Für <strong>';
-			cahoots_content += db[id].name;
-			cahoots_content += '</strong> wurden die folgenden Verbindungen gefunden:</p>';
-			cahoots_content += '<section class="cahoots_middle"><ul id="cahoots_list">';
-			$.each(db[id].cahoots, function(i,v){
-            	cahoots_content += '<li class="cahoots_item"><a target="_blank" title="Mehr Infos zu dieser Organisation" href="';
-            	cahoots_content += v.more_info;
-            	cahoots_content += '">';
-            	cahoots_content += v.name;
-            	cahoots_content += '</a><a target="_blank" class="quelle" href="';
-            	cahoots_content += v.src;
-            	cahoots_content += '">Quelle</a></li>';
-            });
-	        cahoots_content += '</ul></section>';
-	        cahoots_content += '<section class="cahoots_footer">';
-	        cahoots_content += '<a target="_blank" href="http://jonasbergmeier.net/cahoots/contribute.html"><button class="cahoots_button">Informationen eintragen</button></a>';
-	        cahoots_content += '<a target="_blank" href="http://jonasbergmeier.net/cahoots/XYZ.html"><button class="cahoots_button">Fehler melden</button></a>';
-	        cahoots_content += '</section>';
-
-	        return callback(cahoots_content);
-		});
-	}
-
 });
--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -1,11 +1,48 @@
 var data = require("sdk/self").data;
 var pageMod = require("sdk/page-mod");
+var Request = require("sdk/request").Request;
 
 pageMod.PageMod({
-  include: "*",
-  contentScriptFile: [data.url("jquery-1.11.1.min.js"),
-                      data.url("jquery_highlight.js"),
-                      data.url("jquery.tooltipster.min.js"),
-                      data.url("authors.js"),
-                      data.url("content_script.js")]
+	include: "*",
+	contentScriptFile: [data.url("jquery-1.11.1.min.js"),
+                        data.url("jquery_highlight.js"),
+                        data.url("jquery.tooltipster.min.js"),
+                        data.url("authors.js"),
+                        data.url("content_script.js")],
+    contentStyleFile: [data.url("style.css"),
+  					   data.url("tooltipster.css")],
+  	onAttach: function(worker) {
+  		worker.port.on("gotID", function(id) {
+  			console.log("got id from cs: "+id);
+  			console.log(data.url('db.json'));
+  			Request ({
+  				url: data.url('db.json'),
+  				onComplete: function(response) {
+  					var db = response.json;
+  					cahoots_content = '<p class="cahoots_top">Für <strong>';
+  					cahoots_content += db[id].name;
+					cahoots_content += '</strong> wurden die folgenden Verbindungen gefunden:</p>';
+					cahoots_content += '<section class="cahoots_middle"><ul id="cahoots_list">';
+					console.log(db[id].cahoots);
+					for (var v in db[id].cahoots) {
+						console.log(v);
+						cahoots_content += '<li class="cahoots_item"><a target="_blank" title="Mehr Infos zu dieser Organisation" href="';
+        				cahoots_content += db[id].cahoots[v].more_info;
+        				cahoots_content += '">';
+        				cahoots_content += db[id].cahoots[v].name;
+        				cahoots_content += '</a><a target="_blank" class="quelle" href="';
+        				cahoots_content += db[id].cahoots[v].src;
+        				cahoots_content += '">Quelle</a></li>';
+					}
+					cahoots_content += '</ul></section>';
+        			cahoots_content += '<section class="cahoots_footer">';
+        			cahoots_content += '<a target="_blank" href="http://jonasbergmeier.net/cahoots/contribute.html"><button class="cahoots_button">Informationen eintragen</button></a>';
+        			cahoots_content += '<a target="_blank" href="http://jonasbergmeier.net/cahoots/XYZ.html"><button class="cahoots_button">Fehler melden</button></a>';
+        			cahoots_content += '</section>';
+
+  					worker.port.emit('gotContent', cahoots_content);
+  				}
+  			}).get();
+  		});
+  	}
 });
