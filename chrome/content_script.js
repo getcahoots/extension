2f36925c024382634ac382d86d0e9783de8c9455
wuwi <forward.alba@gmail.com>
2014-07-07 01:40:35 +0200

    firefox slow execution workaround

--- chrome/content_script.js
+++ chrome/content_script.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
-	$.each(author, function(key, value) {
-		$("*").highlight(key, {caseSensitive: false, className: value });
-	});
+	//$.each(author, function(key, value) {
+	//	$("*").highlight(key, {caseSensitive: false, className: value });
+	//});
+	
+	for (var key in author) {
+		if (!author.hasOwnProperty(key)) {
+			continue;
+		}
+		if ( $('body:contains("'+key+'")').length > 0 ) {
+			$("*").highlight(key, {caseSensitive: false, className: author[key] });
+		}
+	}
 
 
 	$('span[class*=CahootsID]').tooltipster({
