65b95fdd497b7b10aed6b36a68b73b3a46676927
wuwi <forward.alba@gmail.com>
2014-08-27 11:55:48 +0200

    ff update to 2.4.1

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -6,7 +6,7 @@ pageMod.PageMod({
 	include: "*",
 	contentScriptFile: [data.url("jquery-1.11.1.min.js"),
                         data.url("jquery_highlight.js"),
-                        data.url("jquery.tooltipster.min.js"),
+                        data.url("jquery.tooltipster.js"),
                         data.url("authors.js"),
                         data.url("content_script.js")],
     contentStyleFile: [data.url("style.css"),
