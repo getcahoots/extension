2468c2714847f43da3aa86a2c577636d9eff0bae
Webkodierer <oliver@mashlab.com>
2015-03-06 22:02:31 +0100

    chrome testing content script

---
+++ karma-chrome.conf.js
@@ -0,0 +1,103 @@
+// Karma configuration
+// Generated on Sun Feb 22 2015 20:56:28 GMT+0100 (CET)
+
+module.exports = function (config) {
+    config.set({
+
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
+
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        //frameworks: ['jasmine-jquery', 'jasmine'],
+        frameworks: ['jasmine-jquery','jasmine','commonjs'],
+
+        plugins: [
+            'karma-commonjs',
+            'karma-jasmine',
+            'karma-jasmine-jquery',
+
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-phantomjs-launcher',
+            'karma-nyan-reporter'
+        ],
+
+        // list of files / patterns to load in the browser
+        files: [
+
+            {pattern: 'src/test/js/main-test.js', included: true},
+            {pattern: 'src/test/js/mocks.js', included: true},
+
+
+            {pattern: 'src/main/js/jquery/*js', included: true},
+            {pattern: 'bower_components/tooltipster/js/jquery.tooltipster.js', included: true},
+
+            {pattern: 'target/js/*.js', included: true},
+
+            {pattern: 'src/main/js/chrome/*.js', served: true, included: false},
+
+            {pattern: 'src/test/js/chrome/*.js', included: true},
+
+            //{pattern: 'src/main/js/chrome/**/*js', included: true},
+
+            {pattern: 'src/test/resources/html/*.html', watched: true, served: true, included: true},
+
+        ],
+
+
+        // list of files to exclude
+        exclude: [],
+
+
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        //preprocessors: {},
+
+        preprocessors: {
+            'src/**/*.js': ['commonjs']
+        },
+
+        commonjsPreprocessor: {
+            modulesRoot: 'src/main/js'
+        },
+
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        //reporters: ['progress'],
+        reporters: ['nyan'],
+
+
+        // web server port
+        port: 9876,
+
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: [ 'Chrome']//, ''],
+        //browsers: ['Firefox','Chrome'],
+        //browsers: ['PhantomJS'],
+
+        ,browserNoActivityTimeout: 100000
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        //singleRun: true
+    });
+};
--- src/test/js/chrome/ChromeContentScript.spec.js
+++ src/test/js/chrome/ChromeContentScript.spec.js
@@ -1,42 +1,178 @@
-describe('Chrome Content Script',function suite(){
+describe('Chrome Content Script', function suite() {
     var f;
-
-    var tabFixture= 'src/test/resources/html/chrome-tab-fixture.html';
-
-    beforeEach(function() {
-        //timerCallback = jasmine.createSpy('timerCallback');
-        //jasmine.Clock.useMock();
-
-
-
+    //var chrome;
+    var tabFixture = 'src/test/resources/html/chrome-tab-fixture.html';
+    //var chrome;
+    beforeEach(function () {
         f = jasmine.getFixtures();
         f.fixturesPath = 'base';
         f.load(tabFixture);
+
+        //jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+
+
     });
 
 
-    it('should execute',function(done){
-        //console.log(document)
-        //jQuery('body').append(
-        //    new jQuery('<script/>').attr({type:'application/javascript','src':'/base/target/js/CahootsContentBundle.js'}),
-        //    new jQuery('<script/>').attr({type:'application/javascript','src':'/base/src/main/js/chrome/ChromeContentScript.js'})
-        //)
+    it('should demand authorHints', function () {
+        // ensure element does not exist beforehand
 
+        chrome = getChromeMock();
+        spyOn(chrome.runtime, 'sendMessage');
+
+        // inject content script
+        var cs = require("chrome/ChromeContentScript")
+        cs()
+
+        // fire dom ready
         var event = document.createEvent("HTMLEvents");
         event.initEvent("DOMContentLoaded", true, true);
         document.dispatchEvent(event)
 
-        //var page = new WebPage();
+        expect(chrome.runtime.sendMessage).toHaveBeenCalledWith({message: 'getAuthorHints'}, jasmine.any(Function))
+    })
 
 
+    it('should tooltipster', function () {
+        // ensure element does not exist beforehand
+        var notexistingElement = jQuery("span.CahootsID_c29bca5141c539897b9fb19fc071dd12475e86aa.tooltipstered");
+        expect(typeof notexistingElement.get(0)).toBe('undefined')
 
-        //console.log(phantomjs)
+        // inject content script
+        var cs = require("chrome/ChromeContentScript")
+        cs()
 
-        setTimeout(function(){
+        // fire dom ready
+        var event = document.createEvent("HTMLEvents");
+        event.initEvent("DOMContentLoaded", true, true);
+        document.dispatchEvent(event)
+
+        // check transformed element
+        var expectedElement = jQuery('<span class="CahootsID_c29bca5141c539897b9fb19fc071dd12475e86aa tooltipstered">Stefan Kornelius</span>');
+        var transformed = jQuery("span.CahootsID_c29bca5141c539897b9fb19fc071dd12475e86aa.tooltipstered");
+        expect(transformed.get(0).outerHTML).toBe(expectedElement.get(0).outerHTML);
+    })
+
+    it('should demand authorDetails', function (done) {
+        // ensure element does not exist beforehand
+
+        chrome = getChromeMock();
+        spyOn(chrome.runtime, 'sendMessage').and.callThrough();
+
+        // inject content script
+        var cs = require("chrome/ChromeContentScript")
+        cs()
+
+        // fire dom ready
+        var event = document.createEvent("HTMLEvents");
+        event.initEvent("DOMContentLoaded", true, true);
+        document.dispatchEvent(event)
+
+        expect(chrome.runtime.sendMessage).toHaveBeenCalledWith({message: 'getAuthorHints'},
+            jasmine.any(Function)
+        )
+
+
+        chrome = getChromeMock();
+        spyOn(chrome.runtime, 'sendMessage').and.callThrough();
+
+
+        //var mouseoverEvent= jQuery.Event('mouseover');
+        //event.stopPropagation();
+        expect(jQuery('span.CahootsID_c29bca5141c539897b9fb19fc071dd12475e86aa.tooltipstered')).toBeDefined()
+        //try {
+        //    //jQuery("span.CahootsID_c29bca5141c539897b9fb19fc071dd12475e86aa.tooltipstered").trigger(mouseoverEvent);
+        //} catch(e) {
+        //    console.log(e)
+        //}
+
+        //console.log(transformed);
+        var transformed = document.getElementsByClassName('CahootsID_c29bca5141c539897b9fb19fc071dd12475e86aa tooltipstered').item(0)
+        var mouseoverEvent = document.createEvent("Events");
+        mouseoverEvent.initEvent("mouseover", true, false);
+        transformed.dispatchEvent(mouseoverEvent)
+
+
+        setTimeout(function () {
+            //jQuery("span.CahootsID_c29bca5141c539897b9fb19fc071dd12475e86aa.tooltipstered").onmouseover()
             //console.log(document)
+            expect(chrome.runtime.sendMessage)
+                .toHaveBeenCalledWith({
+                    message: 'getFullDetails',
+                    'cahootsID': 'c29bca5141c539897b9fb19fc071dd12475e86aa'
+                }, jasmine.any(Function))
             done();
-        },500)
+        }, 2000)
+    })
 
+
+    it('should display cahoots popover', function (done) {
+        // ensure element does not exist beforehand
+
+        chrome = getChromeMock();
+        spyOn(chrome.runtime, 'sendMessage').and.callThrough();
+
+        // inject content script
+        var cs = require("chrome/ChromeContentScript")
+        cs()
+
+        // fire dom ready
+        var event = document.createEvent("HTMLEvents");
+        event.initEvent("DOMContentLoaded", true, true);
+        document.dispatchEvent(event)
+
+        expect(chrome.runtime.sendMessage).toHaveBeenCalledWith({message: 'getAuthorHints'},
+            jasmine.any(Function)
+        )
+
+
+        chrome = getChromeMock();
+        spyOn(chrome.runtime, 'sendMessage').and.callThrough();
+
+
+        //var mouseoverEvent= jQuery.Event('mouseover');
+        //event.stopPropagation();
+        expect(jQuery('span.CahootsID_c29bca5141c539897b9fb19fc071dd12475e86aa.tooltipstered')).toBeDefined()
+        //try {
+        //    //jQuery("span.CahootsID_c29bca5141c539897b9fb19fc071dd12475e86aa.tooltipstered").trigger(mouseoverEvent);
+        //} catch(e) {
+        //    console.log(e)
+        //}
+
+        //console.log(transformed);
+        var transformed = document.getElementsByClassName('CahootsID_c29bca5141c539897b9fb19fc071dd12475e86aa tooltipstered').item(0)
+        var mouseoverEvent = document.createEvent("Events");
+        mouseoverEvent.initEvent("mouseover", true, false);
+        transformed.dispatchEvent(mouseoverEvent)
+
+
+        //console.log(document)
+
+
+
+
+
+        var popoverObservedHtml = document.getElementsByClassName('tooltipster-base').item(0).outerHTML
+        var popoverExpectedHtml = '<div class="tooltipster-base tooltipster-default tooltipster-grow tooltipster-grow-show" style="max-width: 344px; pointer-events: auto; -webkit-animation-duration: 210ms; transition: 210ms; -webkit-transition: 210ms; top: 253px; left: 0px;"><div class="tooltipster-content"><div class="cahoots_popover"><p class="cahoots_top">Für <strong><a href="http://de.wikipedia.org/wiki/Stefan_Kornelius" target="_blank" class="name_info"><span class="CahootsID_c29bca5141c539897b9fb19fc071dd12475e86aa tooltipstered">Stefan Kornelius</span></a></strong> wurden die folgenden Verbindungen gefunden:</p><section class="cahoots_middle"><ul id="cahoots_list"><li class="cahoots_item"><a target="_blank" title="Mehr Infos zu dieser Organisation" href="https://www.atlantik-bruecke.org/">Atlantik-Brücke e.V.</a><a target="_blank" href="https://github.com/wuwi/cahoots/blob/master/misc/stefan_kornelius_mail.txt" class="quelle">Quelle</a></li><li class="cahoots_item"><a target="_blank" title="Mehr Infos zu dieser Organisation" href="https://dgap.org/de">Deutsche Gesellschaft für Auswärtige Politik</a><a target="_blank" href="https://github.com/wuwi/cahoots/blob/master/misc/stefan_kornelius_mail.txt" class="quelle">Quelle</a></li><li class="cahoots_item"><a target="_blank" title="Mehr Infos zu dieser Organisation" href="http://de.wikipedia.org/wiki/Bundesakademie_f%C3%BCr_Sicherheitspolitik">Bundesakademie für Sicherheitspolitik</a><a target="_blank" href="https://github.com/wuwi/cahoots/blob/master/misc/stefan_kornelius_mail.txt" class="quelle">Quelle</a></li><li class="cahoots_item"><a target="_blank" title="Mehr Infos zu dieser Organisation" href="http://de.wikipedia.org/wiki/Deutsch-Russisches_Forum">Deutsch-Russisches Forum</a><a target="_blank" href="https://github.com/wuwi/cahoots/blob/master/misc/stefan_kornelius_mail.txt" class="quelle">Quelle</a></li><li class="cahoots_item"><a target="_blank" title="Mehr Infos zu dieser Organisation" href="http://de.wikipedia.org/wiki/K%C3%B6rber-Stiftung">Körber-Stiftung</a><a target="_blank" href="https://web.archive.org/web/20140302133310/http://www.koerber-stiftung.de/koerberforum/gaeste/gaeste-details/gast/stefan-kornelius.html" class="quelle">Quelle</a></li><li class="cahoots_item"><a target="_blank" title="Mehr Infos zu dieser Organisation" href="http://www.aicgs.org/">American Institute for Contemporary German Studies</a><a target="_blank" href="https://web.archive.org/web/20140914130152/http://www.message-online.com/archiv/message-1-2013/leseproben/die-naehe-zur-macht/" class="quelle">Quelle</a></li><li class="cahoots_item"><a target="_blank" title="Mehr Infos zu dieser Organisation" href="http://www.deutscheatlantischegesellschaft.de/cms/front_content.php?idcat=73&amp;lang=1">Deutsche Atlantische Gesellschaft</a><a target="_blank" href="https://web.archive.org/web/20140914130152/http://www.message-online.com/archiv/message-1-2013/leseproben/die-naehe-zur-macht/" class="quelle">Quelle</a></li><li class="cahoots_item"><a target="_blank" title="Mehr Infos zu dieser Organisation" href="http://de.wikipedia.org/wiki/M%C3%BCnchner_Sicherheitskonferenz">Münchner Sicherheitskonferenz</a><a target="_blank" href="https://web.archive.org/web/20140716065613/https://www.securityconference.de/de/veranstaltungen/munich-security-conference/msc-2014/teilnehmer/" class="quelle">Quelle</a></li></ul></section><section class="cahoots_footer"><a target="_blank" href="http://cahoots-extension.github.io/index.html#contribute"><button class="cahoots_button">Verbindung melden</button></a><a target="_blank" href="mailto:mail@cahoots.pw?subject=Fehler"><button class="cahoots_button">Fehler melden</button></a></section></div></div><div class="tooltipster-arrow-bottom tooltipster-arrow" style="left:-44px;"><span style="border-color:rgba(0, 0, 0, 0);"></span></div></div>';
+
+        //console.log(popoverObservedHtml)
+        expect(popoverObservedHtml).toBe(popoverExpectedHtml);
+
+
+        setTimeout(function () {
+            expect(chrome.runtime.sendMessage)
+                .toHaveBeenCalledWith({
+                    message: 'getFullDetails',
+                    'cahootsID': 'c29bca5141c539897b9fb19fc071dd12475e86aa'
+                }, jasmine.any(Function))
+            //jQuery("span.CahootsID_c29bca5141c539897b9fb19fc071dd12475e86aa.tooltipstered").onmouseover()
+            //console.log(document)
+
+
+            //console.log(jQuery('span'))
+
+            done();
+        }, 3000)
 
     })
 })
