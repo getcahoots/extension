afa18077b73c484a353106d970bffed7dee56e62
webkodierer <username@users.noreply.github.com>
2017-08-17 01:28:09 +0200

    #45 webpack, travis

--- webpack.config.js
+++ webpack.config.js
@@ -25,15 +25,20 @@ let config = {
     plugins: [
         new CopyWebpackPlugin([
             // Copy glob results to /absolute/path/
-            {   context: 'src/main/resources',
+            {
+                context: 'src/main/resources',
                 from: '**/*png',
                 to: 'img',
-            flatten: true},
-            {   context: 'src/main/resources',
+                flatten: true
+            },
+            {
+                context: 'src/main/resources',
                 from: '**/*css',
                 to: '.',
-                flatten: true},
-            {   context: 'src',
+                flatten: true
+            },
+            {
+                context: 'src',
                 from: 'manifest.json',
                 to: '.',
                 flatten: true,
@@ -41,13 +46,40 @@ let config = {
                     var data = JSON.parse(content.toString());
                     // store paths in json object or do some other logic
                     data.version = require('./package.json').version;
+                    data.icons['128'] = 'img/cdot_128px.png';
+                    data.icons['48'] = 'img/cdot_48px.png';
+                    data.icons['16'] = 'img/cdot_16px.png';
+                    data.page_action.default_icon['19'] = 'img/cdot_19px.png';
+                    data.page_action.default_icon['38'] = 'img/cdot_38px.png';
                     data.relativePath = relativePath;
                     return new Buffer(JSON.stringify(data));
-                }}
+                }
+            },
+            {
+                context: 'src/options',
+                from: 'options*',
+                to: '.',
+                flatten: true
+            },
+            {
+                context: 'src/jquery',
+                from: '*',
+                to: '.',
+                flatten: true
+            },
+            {
+                from: 'node_modules/jquery/dist/jquery.js',
+                to: '.',
+                flatten: true
+            },
+            {
+                from: 'bower_components/tooltipster/js/jquery.tooltipster.js',
+                to: '.',
+                flatten: true
+            }
         ])
     ]
 }
-
 
 
 module.exports = config;
